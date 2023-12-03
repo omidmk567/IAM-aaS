30fc53bb6e8d2f52179bb13c10cb7fa918cd1b25
o.mohkamkar <mohkamkar@jibit.ir>
2023-12-03 15:53:35 +0330

    Add iam api spring boot base project

---
+++ iam-api/src/main/java/com/omidmk/iamapi/IamApiApplication.java
@@ -0,0 +1,13 @@
+package com.omidmk.iamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamApiApplication.class, args);
+    }
+
+}
